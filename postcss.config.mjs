8edfa5b690d563aae1e2784d5a4fdbe03d90c7aa
Ikhwan Ramadhan <120089389+IngsR@users.noreply.github.com>
2025-04-25 13:46:54 +0700

    Delete postcss.config.mjs

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {}, // Mengaktifkan plugin Tailwind CSS
-    autoprefixer: {}, // Mengaktifkan plugin Autoprefixer untuk menambahkan prefix vendor otomatis
-  },
-};
-
-export default config;
